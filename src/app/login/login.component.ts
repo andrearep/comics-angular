4226c083adca46d68ed6a57048f2a11749fd8063
Andrea Repetto <andrea.repetto@consoft.it>
2021-08-24 15:35:53 +0200

    creata buiness logic login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,16 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  loginForm= new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email] ),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    this.loginForm.reset();
   }
 
 }
