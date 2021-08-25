82fd77b3b6bb08eee8603a8bfcd9bfecdbd96cea
Andrea Repetto <andrea.repetto@consoft.it>
2021-08-25 09:29:20 +0200

    modifiche minori

---
+++ src/assets/assets.ts
@@ -0,0 +1,44 @@
+export const comics = [
+  {
+    "id": 0,
+    "title": 'Naruto',
+    "imgPath": 'https://s1.gaming-cdn.com/images/products/8376/271x377/gioco-nintendo-naruto-shippuden-ultimate-ninja-storm-3-full-burst-hd-switch-cover.jpg',
+    "description": ''
+  },
+  {
+    "id": 1,
+    "title": 'DragonBall Z',
+    "imgPath": 'https://www.fumetto-online.it/ew/ew_albi/images/STAR%20COMICS/4674.1.jpg',
+    "description": ''
+  },
+  {
+    "id": 2,
+    "title": 'One Piece',
+    "imgPath": 'https://orgoglionerd.it/wp-content/uploads/2021/04/One-Piece-673x1024.jpg',
+    "description": ''
+  },
+  {
+    "id": 3,
+    "title": 'Seven Deadly Sins',
+    "imgPath": 'https://www.fumetto-online.it/ew/ew_albi/images/STAR%20COMICS/TheSevenDeadlySins11.jpg',
+    "description": ''
+  },
+  {
+    "id": 4,
+    "title": 'Attacco dei giganti',
+    "imgPath": 'https://upload.wikimedia.org/wikipedia/it/4/4d/L%27attacco_dei_giganti_copertina.jpeg',
+    "description": ''
+  },
+  {
+    "id": 5,
+    "title": 'DragonBall GT',
+    "imgPath": 'https://www.starcomics.com/files/immagini/fumetti-cover/db-gt-draghimalvagi1-1200px',
+    "description": ''
+  },
+  {
+    "id": 6,
+    "title": 'Spiderman',
+    "imgPath": 'https://images-na.ssl-images-amazon.com/images/I/61fuaEo9TNL._SX331_BO1,204,203,200_.jpg',
+    "description": ''
+  }
+];
